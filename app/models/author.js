85db58547835fb054490cc15cf7145739ed95165
Ryan Tablada <ryan.tablada@gmail.com>
2017-09-25 19:52:52 -0500

    08-ember-data-findall

---
+++ app/models/author.js
@@ -0,0 +1,6 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  first: DS.attr(),
+  last: DS.attr()
+});
--- app/routes/author.js
+++ app/routes/author.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return fetch('http://localhost:3000/authors')
-      .then(response => response.json());
+    return this.store.findAll('author');
   }
 });
