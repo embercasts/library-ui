1da01ad81fc4b0a62be36039e82cc1a2218b8945
Ryan Tablada <ryan.tablada@gmail.com>
2017-09-26 22:10:40 -0500

    12-route-params

--- app/routes/author/detail.js
+++ app/routes/author/detail.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model({ id }) {
+    return {
+      first: 'J.K.',
+      last: 'Rowling',
+      id: `authors-${id}`
+    };
+  }
 });
