1d1d9305a2c62796d1e71ff4c78295b857bbbebd
Ryan Tablada <ryan.tablada@gmail.com>
2017-10-31 13:01:03 -0500

    vid: find-record

--- app/routes/author/detail.js
+++ app/routes/author/detail.js
@@ -2,10 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model({ id }) {
-    return {
-      first: 'J.K.',
-      last: 'Rowling',
-      id: `authors-${id}`
-    };
+    return this.store.findRecord('author', id);
   }
 });
