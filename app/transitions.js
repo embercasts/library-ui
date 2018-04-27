b6b4adc3010824983a26353326edecaa78f25cd7
Ryan Tablada <ryan.tablada@gmail.com>
2018-04-27 07:27:25 -0500

    Liquid Fire Outlets

--- app/transitions.js
+++ app/transitions.js
@@ -1,8 +1,22 @@
+const duration = 600;
+
 export default function(){
 
   this.transition(
     this.hasClass('modal-if'),
-    this.use('fade', { duration: 600 })
+    this.use('fade', { duration })
+  );
+
+  this.transition(
+    this.fromRoute('book.detail.index'),
+    this.use('toLeft', { duration }),
+    this.reverse('toRight', { duration })
+  );
+
+  this.transition(
+    this.fromRoute('author.detail.index'),
+    this.use('fade', { duration }),
+    this.reverse('fade', { duration })
   );
 
   // Add your transitions here, like:
